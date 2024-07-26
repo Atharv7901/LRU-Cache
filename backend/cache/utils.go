fbbd8b3bbfd57ea18d1e128c93e768c09db764b0
Atharv7901 <atharvshivarkar@gmail.com>
2024-07-27 01:39:28 +0530

    go routine for expiration time

--- backend/cache/utils.go
+++ backend/cache/utils.go
@@ -1,6 +1,9 @@
 package cache
 
-import "container/list"
+import (
+	"container/list"
+	"time"
+)
 
 func (c *LRUCache) removeOldest() {
 	elem := c.CacheData.Back()
@@ -13,4 +16,20 @@ func (c *LRUCache) removeElement(elem *list.Element) {
 	c.CacheData.Remove(elem)
 	item := elem.Value.(*CacheItem)
 	delete(c.Items, item.Key)
+}
+
+func (c *LRUCache) StartExpirationTime() {
+	ticker := time.NewTicker(time.Second)
+	go func()  {
+		for range ticker.C {
+			c.mu.Lock()
+			for _, elem := range c.Items {
+				item := elem.Value.(*CacheItem)
+				if time.Now().Unix() > item.Expiration {
+					c.removeElement(elem)
+				}
+			}
+			c.mu.Unlock()
+		}	
+	}()
 }
