588121d1182a308f7927d1a5a76a2d5f17cb01a3
Atharv7901 <atharvshivarkar@gmail.com>
2024-07-27 11:49:19 +0530

    Set and delete handler

--- backend/pkg/cache/handler.go
+++ backend/pkg/cache/handler.go
@@ -1,13 +1,21 @@
 package cache
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type CacheHandler struct {
 	Cache *LRUCache
+}
+
+type SetRequest struct {
+	Key string `json:"key"`
+	Value string `json:"value"`
+	Expiration int `json:"expiration"`
 }
 
 func NewCacheHandler(capacity int) *CacheHandler {
@@ -27,4 +35,27 @@ func (h *CacheHandler) GetHandler(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, value)
+}
+
+func (h *CacheHandler) SetHandler(c *gin.Context) {
+	var req SetRequest
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error" : "Invalid Body Request"})
+		return
+	}
+
+	if(req.Key == "") || (req.Value == "") {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Key and value are required"})
+		return
+	}
+
+	h.Cache.Set(req.Key, req.Value, time.Duration(req.Expiration)*time.Second)
+	c.Status(http.StatusOK)
+}
+
+func (h *CacheHandler) DeleteHandler(c *gin.Context) {
+	key := c.Query("key")
+	fmt.Println("inside this habdler")
+	h.Cache.Delete(key)
+	c.Status(http.StatusOK)
 }
