561a4e71029b699a15e804de1c30a51aa2d10e8b
Atharv7901 <atharvshivarkar@gmail.com>
2024-07-28 02:27:03 +0530

    Implemented Cache list

---
+++ frontend/src/components/CacheDisplay.jsx
@@ -0,0 +1,73 @@
+import {useEffect, useState} from "react";
+import CacheList from "./CacheList";
+import {Button, Grid} from "@mui/material";
+import CacheForm from "./CacheForm";
+import CacheGetItem from "./CacheGetItem";
+
+const CacheDisplay = () => {
+  const [cacheState, setCacheState] = useState({});
+  const [dialogOpen, setDialogOpen] = useState(false);
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000/ws");
+
+    ws.onopen = () => {
+      console.log("Websocket connection established");
+    };
+
+    ws.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        setCacheState(data);
+      } catch (error) {
+        console.error("Error parsing WebSocket message", error);
+      }
+    };
+
+    ws.onerror = (error) => {
+      console.error("Websocket error:", error);
+    };
+
+    ws.onclose = (event) => {
+      console.log("Websocker connection closed:", event);
+    };
+
+    return () => {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.close();
+      }
+    };
+  }, []);
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+  return (
+    <div>
+      <h1>LRU Display</h1>
+      <Grid container>
+        <Grid item xs={9}>
+          <CacheList data={cacheState} />
+        </Grid>
+        <Grid item xs={1}>
+          <div></div>
+        </Grid>
+        <Grid item xs={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleDialogOpen}
+          >
+            Add Cache Item
+          </Button>
+        </Grid>
+      </Grid>
+      <CacheForm open={dialogOpen} handleClose={handleDialogClose} />
+      <CacheGetItem />
+    </div>
+  );
+};
+
+export default CacheDisplay;
---
+++ frontend/src/components/CacheList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import CacheItem from "./CacheItem";
+import {useDeleteCacheItemMutation} from "../services/cacheApi";
+
+const CacheList = (props) => {
+  const [deleteCacheItem, DeleteCacheItemResponse] =
+    useDeleteCacheItemMutation();
+  const handleDelete = (keyItem) => {
+    console.log("Key", keyItem);
+    deleteCacheItem(keyItem);
+  };
+  return (
+    <div>
+      <h4>Cache state</h4>
+      {props.data &&
+      typeof props.data === "object" &&
+      Object.keys(props.data).length > 0 ? (
+        Object.keys(props.data).map((key) => (
+          <CacheItem
+            key={key}
+            index={key}
+            keyItem={props.data[key].key}
+            value={props.data[key].value}
+            expiration={props.data[key].expiration}
+            onDelete={() => handleDelete(props.data[key].key)}
+          />
+        ))
+      ) : (
+        <div>No Cache state available</div>
+      )}
+    </div>
+  );
+};
+
+export default CacheList;
